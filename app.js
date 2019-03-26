e41a899ed81059c3ff8e318c87ce35745c2ddc27
maz222 <benrollins91@gmail.com>
2019-03-26 16:45:41 -0700

    .

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 if(process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'frontend/build')));
+	app.use(express.static(path.join(__dirname, 'client/build')));
 	app.get('*', function(req, res) {
-		res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+		res.sendFile(path.join(__dirname, '/client/build/index.html'));
 	});
 }
 
