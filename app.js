52b6524b9609c254bb42dce09e8432749ddb8ccf
maz222 <benrollins91@gmail.com>
2019-03-26 20:53:08 -0700

    .

--- app.js
+++ app.js
@@ -18,13 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-if(process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'client/build')));
-	app.get('*', function(req, res) {
-		res.sendFile(path.join(__dirname + '/client/build/index.html'));
-	});
-}
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -76,7 +69,9 @@ io.on('connection', (socket) => {
 	})
 });
 
-
-
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 module.exports = app;
