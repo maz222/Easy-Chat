50d47e70b8fa6b22eb060f7a8874c68fb57cee01
maz222 <benrollins91@gmail.com>
2019-03-26 20:56:36 -0700

    x

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
 
 
 // catch 404 and forward to error handler
